ad85440d46ec3bf7f4488ea6502edfcae411f13c
adhull033 <71253930+adhull033@users.noreply.github.com>
2023-03-07 13:29:51 +0530

    Add files via upload

---
+++ brand.php
@@ -0,0 +1,8 @@
+<?php
+	$domainName = "2pdfonline.com";
+	$domainURL = "https://2pdfonline.com/png-to-jpg";
+	$icon_loc = "./img/icon.png";
+	$main_title = "Compress JPG / JPEG Images Online for Free";  // Title of the Site
+	
+	$contact_email = ",";
+?>
